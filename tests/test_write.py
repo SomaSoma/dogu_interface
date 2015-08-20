8c0ca335066b1629e40e7efe01ba9d5ee8d2a6c7
Luavis <luaviskang@gmail.com>
2015-08-20 12:25:44 +0900

    Add test_write

---
+++ tests/test_write.py
@@ -0,0 +1,28 @@
+
+def test_write(dogu_handler):
+    def application(environ, start_response):
+
+        write = start_response('200 OK', [()])
+        assert hasattr(write, '__call__')
+
+        write(bytearray(10))
+
+        return bytearray()
+
+    dogu_handler(application)
+
+
+def test_invalid_write(dogu_handler):
+    def application(environ, start_response):
+
+        try:
+            write = start_response('200 OK', [()])
+            write(0)  # write int is invalid
+        except ValueError:
+            assert True
+        else:
+            assert False  # status is not str but it doesn't make error
+
+        return bytearray()
+
+    dogu_handler(application)
